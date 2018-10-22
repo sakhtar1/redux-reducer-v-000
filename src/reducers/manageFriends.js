2a7e37ee9a9f8c8be8e00a42cc6220c2e892f588
Sharmina Akhtar <sakht318@gmail.com>
2018-10-22 19:26:12 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
+
+
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      let newFriend = [...state.friends]
+      newFriend.push(action.friend)
+      return {friends: newFriend};
+    case 'REMOVE_FRIEND':
+      let oldFriend = [...state.friends]
+      let indexFriend = oldFriend.findIndex(function(obj){
+        return obj.id === action.id
+      })
+      oldFriend.splice(indexFriend, 1)
+      return {friends: oldFriend}
+    default:
+      return state;
+  }
 }
